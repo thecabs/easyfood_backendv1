79e78a68c1ce007c67c3e97c9662823fe185145b
thecabs <jaurescabreldongmo@gmail.com>
2025-01-14 07:31:43 +0100

    liste entreprise

--- routes/api.php
+++ routes/api.php
@@ -78,20 +78,17 @@ Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/assurances', [AssuranceController::class, 'index']); // Liste des assurances
     Route::get('/assurances/{id}', [AssuranceController::class, 'show']); // Voir une assurance
     Route::post('/assurances', [AssuranceController::class, 'store']); // Créer une assurance
-<<<<<<< HEAD
-
+ 
     Route::post('/assurances/{id}', [AssuranceController::class, 'update']); // Modifier une assurance
     Route::post('/assurances/{id}', [AssuranceController::class, 'update']); // Créer une assurance
     Route::put('/assurances/{id}', [AssuranceController::class, 'update']); // Modifier une assurance
      Route::delete('/assurances/{id}', [AssuranceController::class, 'destroy']); // Supprimer une assurance 
-=======
-    Route::post('/assurances/{id}', [AssuranceController::class, 'update']); // Modifier une assurance
+     Route::post('/assurances/{id}', [AssuranceController::class, 'update']); // Modifier une assurance
     Route::post('/assurances/{id}', [AssuranceController::class, 'update']); // Créer une assurance
     Route::put('/assurances/{id}', [AssuranceController::class, 'update']); // Modifier une assurance
     Route::delete('/assurances/{id}', [AssuranceController::class, 'destroy']); // Supprimer une assurance 
->>>>>>> 471ff3d58b7896a9e9326b04867e564986dc494d
+ 
 });
-
 
  
  Route::middleware(['auth:sanctum'])->group(function () {
@@ -352,9 +349,4 @@ Route::middleware('auth:sanctum')->post('/users/search-by-role', [UserController
 
 
  
-
-
- 
-
-
 
