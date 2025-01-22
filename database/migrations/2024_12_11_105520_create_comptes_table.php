6e1997dc969e94d5d9d83dbe84a11fb31699ba78
elkozu <nzukoemmanuel359@gmail.com>
2025-01-22 09:53:54 +0100

    modification
    j'ai ajouté id°compte dans compte table

--- database/migrations/2024_12_11_105520_create_comptes_table.php
+++ database/migrations/2024_12_11_105520_create_comptes_table.php
@@ -9,7 +9,7 @@ class CreateComptesTable extends Migration
     public function up()
     {
         Schema::create('comptes', function (Blueprint $table) {
-            $table->id(); // Clé primaire auto-incrémentée
+            $table->id('id_compte'); // Clé primaire auto-incrémentée
             $table->string('numero_compte')->unique(); // Numéro de compte unique
             $table->decimal('solde', 15, 2)->default(0.00);
             $table->date('date_creation');
