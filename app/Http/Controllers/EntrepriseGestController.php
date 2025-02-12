42b1a597813f84a1d500138f7abe04eff4545064
elkozu <nzukoemmanuel359@gmail.com>
2025-02-12 15:23:06 +0100

    j'ai ajoute 'adresse','ville','quartier', dans get gestionnaire d'entreprise

--- app/Http/Controllers/EntrepriseGestController.php
+++ app/Http/Controllers/EntrepriseGestController.php
@@ -32,7 +32,7 @@ class EntrepriseGestController extends Controller
             $gestionnaire = User::where('id_user', $id_user)
                 ->where('role', 'entreprise_gest')
                 ->with(['entreprise' => function ($query) {
-                    $query->select('id_entreprise', 'nom', 'secteur_activite', 'id_gestionnaire','id_assurance','logo');
+                    $query->select('id_entreprise', 'nom', 'secteur_activite','adresse','ville','quartier', 'id_gestionnaire','id_assurance','logo');
                 }, 'compte'])
                 ->first();
 
